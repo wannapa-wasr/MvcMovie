9691451008f0ca5bcbfbed2b34228acfc80953bc
Wannapa Srijermtong <wannapaa_s@hotmail.com>
2020-07-07 16:33:21 +0700

    Insert Update Delete with LocalDB

--- Migrations/20200707050946_InitialCreate.cs
+++ Migrations/20200707050946_InitialCreate.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace MvcMovie.Migrations
 {
     public partial class InitialCreate : Migration
-    {
+    {   
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.CreateTable(
