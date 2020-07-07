a21234f791d7071f2fb2f982a466fd68c8c3911d
Wannapa Srijermtong <wannapaa_s@hotmail.com>
2020-07-07 17:58:05 +0700

    Search and Filter data in database

--- MoviesController.cs
+++ MoviesController.cs
@@ -22,20 +22,23 @@ namespace MvcMovie
         // GET: Movies
         public async Task<IActionResult> Index(string movieGenre, string searchString)
         {
-            // Use LINQ to get list of genres.
             IQueryable<string> genreQuery = from m in _context.Movie
                                             orderby m.Genre
                                             select m.Genre;
+
             var movies = from m in _context.Movie
                          select m;
-            if (!String.IsNullOrEmpty(searchString))
+
+            if (!string.IsNullOrEmpty(searchString))
             {
                 movies = movies.Where(s => s.Title.Contains(searchString));
             }
-            if (!String.IsNullOrEmpty(movieGenre))
+
+            if (!string.IsNullOrEmpty(movieGenre))
             {
-                movies = movies.Where(x => x.Title.Contains(movieGenre));
+                movies = movies.Where(x => x.Genre == movieGenre);
             }
+
             var movieGenreVM = new MovieGenreViewModel
             {
                 Genres = new SelectList(await genreQuery.Distinct().ToListAsync()),
